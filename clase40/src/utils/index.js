949df359958f3bdb2840d54ffaff307892a46199
MaviChillo <mavi.chillo@gmail.com>
2023-05-23 20:57:47 -0300

    subida clase 40

---
+++ clase40/src/utils/index.js
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt';
+import {fileURLToPath} from 'url';
+import { dirname, join } from 'path';
+
+export const createHash = async(password) =>{
+    const salts = await bcrypt.genSalt(10);
+    return bcrypt.hash(password,salts);
+}
+
+export const passwordValidation = async(hashPassword,password) => bcrypt.compare(hashPassword,password);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(dirname(__filename), '..')
+
+export default __dirname;
